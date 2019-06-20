acd4423b30772059888bf370e8fb63bfd6bbfcbb
rubensbezerra29 <52016800+rubensbezerra29@users.noreply.github.com>
2019-06-19 21:57:28 -0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,17 +2,7 @@ const Discord = require("discord.js"); //baixar a lib
 const client = new Discord.Client(); 
 const config = require("./config.json"); 
 
-//heroku
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 client.on("ready", () => {
   console.log(`Bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`); 
